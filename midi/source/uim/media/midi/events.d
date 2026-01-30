81a7e494b4cfa9423b79ab6e0a414e5375ff93ff
ONS <ui.manufaktur@gmail.com>
2026-01-30 13:43:43 +0100

    Aktualisieren von events.d

--- midi/source/uim/media/midi/events.d
+++ midi/source/uim/media/midi/events.d
@@ -446,29 +446,3 @@ class EndOfTrackEvent : MetaEvent {
     }
 }
 
-/**
- * System Exclusive (SysEx) event
- */
-class SysExEvent : MIDIEvent {
-    ubyte[] data;
-    
-    this(uint deltaTime, ubyte[] data) @safe {
-        super(deltaTime);
-        this.data = data.dup;
-    }
-    
-    override MIDIEventType getEventType() const @safe {
-        return MIDIEventType.sysEx;
-    }
-    
-    override ubyte[] toBytes() const @safe {
-        ubyte[] result = [0xF0];
-        result ~= TextEvent.encodeVariableLength(data.length);
-        result ~= data;
-        return result;
-    }
-    
-    override size_t getSize() const @safe {
-        return 1 + TextEvent.variableLengthSize(data.length) + data.length;
-    }
-}
