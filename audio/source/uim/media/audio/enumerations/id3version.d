e127252f37f7c162d1d855132ab5cdf2ced42b22
ONS <ui.manufaktur@gmail.com>
2026-01-30 11:59:55 +0100

    id3version.d erstellen

---
+++ audio/source/uim/media/audio/enumerations/id3version.d
@@ -0,0 +1,21 @@
+/*********************************************************************************************************
+	Copyright: © 2018-2026 Ozan Nurettin Süel (aka UIManufaktur)
+	License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.
+	Authors: Ozan Nurettin Süel (aka UIManufaktur)
+**********************************************************************************************************/
+module uim.media.audio.enumerations.id3version;
+
+import uim.media.audio;
+@safe:
+
+/**
+ * ID3 tag version enumeration
+ */
+enum ID3Version {
+    none,
+    v1,
+    v1_1,
+    v2_2,
+    v2_3,
+    v2_4
+}
