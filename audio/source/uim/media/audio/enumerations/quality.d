82ea354a18f22c0e47ada3087b16843e87091404
ONS <ui.manufaktur@gmail.com>
2026-01-30 12:02:40 +0100

    quality.d erstellen

---
+++ audio/source/uim/media/audio/enumerations/quality.d
@@ -0,0 +1,14 @@
+module uim.media.audio.enumerations.quality;
+
+import uim.media.audio;
+@safe:
+/**
+ * Audio quality preset
+ */
+enum AudioQuality {
+    low,
+    medium,
+    high,
+    veryhigh,
+    lossless
+}
