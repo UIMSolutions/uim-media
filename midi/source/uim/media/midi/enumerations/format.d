d3500f4e2e57700d91b82289a51fca9d24d35557
ONS <ui.manufaktur@gmail.com>
2026-01-30 13:46:29 +0100

    enumerations/format.d erstellen

---
+++ midi/source/uim/media/midi/enumerations/format.d
@@ -0,0 +1,8 @@
+/**
+ * MIDI file format types
+ */
+enum MIDIFormat {
+    format0 = 0,  /// Single multi-channel track
+    format1 = 1,  /// Multiple simultaneous tracks
+    format2 = 2   /// Multiple independent tracks
+}
