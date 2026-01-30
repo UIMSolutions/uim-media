9d0fd4b165e083a0da6b7ee8da17f2d358bba99b
ONS <ui.manufaktur@gmail.com>
2026-01-30 13:48:46 +0100

    Aktualisieren von format.d

--- midi/source/uim/media/midi/enumerations/format.d
+++ midi/source/uim/media/midi/enumerations/format.d
@@ -1,3 +1,13 @@
+/*********************************************************************************************************
+	Copyright: © 2018-2026 Ozan Nurettin Süel (aka UIManufaktur)
+	License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.
+	Authors: Ozan Nurettin Süel (aka UIManufaktur)
+**********************************************************************************************************/
+module uim.media.midi.enumerations.format;
+
+import uim.media.midi;
+@safe:
+
 /**
  * MIDI file format types
  */
