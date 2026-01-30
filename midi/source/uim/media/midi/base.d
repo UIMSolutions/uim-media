d99084b39f97979206242db4bc5f7790e188fcf7
ONS <ui.manufaktur@gmail.com>
2026-01-30 13:52:57 +0100

    Aktualisieren von base.d

--- midi/source/uim/media/midi/base.d
+++ midi/source/uim/media/midi/base.d
@@ -9,27 +9,6 @@ import uim.media.midi;
 @safe:
 
 /**
- * MIDI meta event types
- */
-enum MIDIMetaEventType : ubyte {
-    sequenceNumber = 0x00,
-    text = 0x01,
-    copyright = 0x02,
-    trackName = 0x03,
-    instrumentName = 0x04,
-    lyric = 0x05,
-    marker = 0x06,
-    cuePoint = 0x07,
-    channelPrefix = 0x20,
-    endOfTrack = 0x2F,
-    tempo = 0x51,
-    smpteOffset = 0x54,
-    timeSignature = 0x58,
-    keySignature = 0x59,
-    sequencerSpecific = 0x7F
-}
-
-/**
  * MIDI controller numbers
  */
 enum MIDIController : ubyte {
