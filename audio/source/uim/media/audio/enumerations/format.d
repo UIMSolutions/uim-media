4e46522ebff7ba18276426b137094447dbfd3f5b
ONS <ui.manufaktur@gmail.com>
2026-01-30 11:53:35 +0100

    format.d erstellen

---
+++ audio/source/uim/media/audio/enumerations/format.d
@@ -0,0 +1,30 @@
+/*********************************************************************************************************
+	Copyright: © 2018-2026 Ozan Nurettin Süel (aka UIManufaktur)
+	License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.
+	Authors: Ozan Nurettin Süel (aka UIManufaktur)
+**********************************************************************************************************/
+module uim.media.audio.enumerations.format;
+
+import uim.media.audio;
+@safe:
+
+/**
+ * Audio format enumeration
+ */
+enum AudioFormat {
+    unknown,
+    mp3,
+    wav,
+    flac,
+    ogg,
+    aac,
+    m4a,
+    wma,
+    opus,
+    ape,
+    alac,
+    aiff,
+    au,
+    mid,
+    midi
+}
