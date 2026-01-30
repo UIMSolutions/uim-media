ba5ad370a777cc387989c5a8a4a657a01c1539ef
ONS <ui.manufaktur@gmail.com>
2026-01-30 13:49:47 +0100

    Aktualisieren von base.d

--- midi/source/uim/media/midi/base.d
+++ midi/source/uim/media/midi/base.d
@@ -9,39 +9,6 @@ import uim.media.midi;
 @safe:
 
 /**
- * MIDI event types
- */
-enum MIDIEventType : ubyte {
-    // Channel voice messages
-    noteOff = 0x80,
-    noteOn = 0x90,
-    polyKeyPressure = 0xA0,
-    controlChange = 0xB0,
-    programChange = 0xC0,
-    channelPressure = 0xD0,
-    pitchBend = 0xE0,
-    
-    // System common messages
-    sysEx = 0xF0,
-    timeCodeQuarterFrame = 0xF1,
-    songPositionPointer = 0xF2,
-    songSelect = 0xF3,
-    tuneRequest = 0xF6,
-    endOfSysEx = 0xF7,
-    
-    // System real-time messages
-    timingClock = 0xF8,
-    start = 0xFA,
-    continue_ = 0xFB,
-    stop = 0xFC,
-    activeSensing = 0xFE,
-    systemReset = 0xFF,
-    
-    // Meta event
-    meta = 0xFF
-}
-
-/**
  * MIDI meta event types
  */
 enum MIDIMetaEventType : ubyte {
