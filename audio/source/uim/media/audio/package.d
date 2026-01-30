09bb65301c52fedc8fb6d11e7a151eb0bfd44ee3
ONS <ui.manufaktur@gmail.com>
2026-01-30 11:58:31 +0100

    Aktualisieren von package.d

--- audio/source/uim/media/audio/package.d
+++ audio/source/uim/media/audio/package.d
@@ -7,10 +7,8 @@ module uim.media.audio;
 
 // Main audio module - UIM Media Audio Library
 public {
-  import uim.media.audio.base;
-  import uim.media.audio.formats;
-  import uim.media.audio.metadata;
-  import uim.media.audio.codec;
-  import uim.media.audio.io;
-  import uim.media.audio.tags;
+  import uim.media.audio.classes;
+  import uim.media.audio.exceptions;
+  import uim.media.audio.interfaces;
+  import uim.media.audio.helpers;
 }
